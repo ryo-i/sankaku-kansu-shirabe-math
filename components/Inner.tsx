ad68d16a9b535fcd6497c388bbde3a8d52598c62
ryo-i <iidazz0109@gmail.com>
2021-12-05 06:07:49 +0900

    React、p5.js間の値の受け渡しリファクタリング #3

--- components/Inner.tsx
+++ components/Inner.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect }  from 'react';
 import styled from 'styled-components';
 import { P5WrapperProps } from 'react-p5-wrapper';
 import sketch from '../modules/sketch/unitCircle';
-import { p2rText, timesClicked } from '../modules/sketch/unitCircle';
+import { p2rText, countClicks } from '../modules/sketch/unitCircle';
 import dynamic from "next/dynamic";
 import { inner } from '../data/data.json';
 
@@ -27,30 +27,30 @@ const H2 = styled.h2`
 // Component
 function Inner() {
   // Hooks
-  const [title, setTitle] = useState('内容が無いよう');
+  const [title, setTitle] = useState('値の受け渡しテスト');
   const [text, setText] = useState('へんじがない、ただのしかばねのようだ。');
   const [clickTimes, setClickTimes] = useState(0);
+  const [countTimes, setCountTimes] = useState(0);
 
   useEffect(() => {
     if (process.browser) {
-      document.body.onkeyup = function(e){
-        if(e.keyCode == 32){
-            console.log(p2rText);
-            console.log(timesClicked);
-            setText(p2rText);
-            setClickTimes(timesClicked);
-        }
+      document.body.onclick = function(e){
+        setText(p2rText);
+        setClickTimes(countClicks + 1);
       }
     }
 
-    /* const interval = setInterval(
-      () => setRotation(rotation => rotation + 100),
-      100
+    let countUp: number;
+    const interval = setInterval(
+      () => {
+        setCountTimes(countTimes => countTimes + 1);
+      },
+      1000
     );
 
     return () => {
       clearInterval(interval);
-    }; */
+    };
   }, []);
 
   // JSX
@@ -61,11 +61,11 @@ function Inner() {
           <ReactP5Wrapper
             sketch={sketch}
             r2pText={data.text}
+            countTimes={countTimes}
           />
         </figure>
         <h2>{ title }</h2>
         <p>{ text }</p>
-        <p>※スペースをおしてみて！</p>
         <p>あなたのクリック数：{ clickTimes }</p>
       </section>
     </>
--- modules/sketch/unitCircle.ts
+++ modules/sketch/unitCircle.ts
@@ -1,12 +1,12 @@
 import { P5Instance } from 'react-p5-wrapper'
 
 
-export let p2rText: string = '';
-export let timesClicked = 0;
+export let p2rText: string = 'p5.jsからReactへ';
+export let countClicks = 0;
 
 
 const unitCircle = (p5: P5Instance) => {
-    let rotation: number = 0;
+    let countTimes: number = 0;
     let r2pText: string = '';
 
     p5.setup = () => {
@@ -21,18 +21,17 @@ const unitCircle = (p5: P5Instance) => {
     };
 
     p5.updateWithProps = (props) => {
-        if (props.rotation) {
-            rotation = (props.rotation * Math.PI) / 180;
-        }
-
         if (props.r2pText) {
             r2pText = props.r2pText;
+        }
+
+        if (props.countTimes) {
+            countTimes = props.countTimes;
         }
     };
 
     p5.mouseClicked = function() {
-        p2rText = 'p5.jsからReactへ';
-        timesClicked++;
+        countClicks++;
     }
 
     p5.draw = () => {
@@ -40,6 +39,7 @@ const unitCircle = (p5: P5Instance) => {
         p5.ellipse(50, 50, 80, 80);
         p5.textSize(32);
         p5.text(r2pText, 10, 30);
+        p5.text(countTimes + '秒経過', 10, 70);
     };
 
     p5.windowResized = () => {
